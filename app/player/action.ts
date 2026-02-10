abb027cff14cac6b6d4c43646e1357abaa8869f4
Warodom Werapun <wwarodom@gmail.com>
2026-02-10 11:05:06 +0700

    display errror message

--- app/player/action.ts
+++ app/player/action.ts
@@ -1,3 +1,12 @@
+import { z } from 'zod'
+
+const PlayerSchema = z.object({
+    name: z.string().min(3).max(10),
+    age: z.number().min(18, "Need to grow up baby!!")
+        .max(60, "You are too old now!"),
+    email: z.email().optional()
+})
+
 export default function submitPlayer(
     prevState: unknown, formData: FormData) {
 
@@ -7,10 +16,20 @@ export default function submitPlayer(
 
     const data = { name, age, email }
 
-    console.log(data)
+
+    const result = PlayerSchema.safeParse(data)
+    console.log(result)
+
+    if (!result.success) {
+        console.log(z.treeifyError(result.error))
+        return {
+            errors: z.treeifyError(result.error).properties,
+            data
+        }
+    }
 
     return {
-        error: {},
+        errors: {},
         data
     }
 }
--- app/player/page.tsx
+++ app/player/page.tsx
@@ -3,7 +3,7 @@ import { useActionState } from "react"
 import submitPlayer from "./action"
 export default function PlayerPage() {
     const initState = {
-        error: {},
+        errors: {},
         data: {
             name: "",
             age: 0,
@@ -18,27 +18,40 @@ export default function PlayerPage() {
             <h1>Player Page</h1>
 
             <pre className="border border-amber-500 p-2 m-2">
-                {JSON.stringify(state.data, null, 4)}
+                {JSON.stringify(state, null, 4)}
             </pre>
 
             <form action={action}>
                 <div>
-                    Name: <input className="border my-2 px-2"
+                    Name:
+                    <input className="border my-2 px-2"
                         type="text" name="name"
                         defaultValue={state.data.name}
                     />
+                    {state.errors?.name &&
+                        <p className="text-sm text-red-600"> {
+                            state.errors?.name.errors[0]
+                        } </p>}
                 </div>
                 <div>
                     Age: <input className="border my-2 px-2"
                         type="number" name="age"
                         defaultValue={state.data.age}
                     />
+                    {state.errors?.age &&
+                        <p className="text-sm text-red-600"> {
+                            state.errors?.age.errors[0]
+                        } </p>}
                 </div>
                 <div>
                     Email: <input className="border my-2 px-2"
                         type="email" name="email"
                         defaultValue={state.data.email}
                     />
+                    {state.errors?.email &&
+                        <p className="text-sm text-red-600"> {
+                            state.errors?.email.errors[0]
+                        } </p>}
                 </div>
                 <div>
                     <button className="border my-2 px-2">Submit</button>
